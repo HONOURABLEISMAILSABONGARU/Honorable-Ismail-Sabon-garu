738b505511bd55767375aec326fb163ac0b7c4e8
HONOURABLEISMAILSABONGARU <banasoyayya01@gmail.com>
2026-07-31 21:31:33 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("HONOURABLE ISMAIL SABON GARU Registration Portal Loaded Successfully");
-});
+
+  const form = document.getElementById("registrationForm");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const appId = "TTT-SBNGR-2027-APP-00121";
+
+alert(
+  "Application Submitted Successfully!\n\n" +
+  "Application ID: " + appId + "\n\n" +
+  "IMPORTANT NOTICE:\n" +
+  "Please save your Application ID or print it. You will be required to present it when collecting your ID Card."
+);
