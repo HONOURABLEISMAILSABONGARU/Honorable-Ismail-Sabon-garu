090f3ed3807dff9d14656b99a160c269c890dfcc
HONOURABLEISMAILSABONGARU <banasoyayya01@gmail.com>
2026-08-02 13:42:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const form = document.getElementById("registrationForm");
 
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Generate Application ID
+    const applicationId = "TTT-" + Math.floor(100000 + Math.random() * 900000);
+
+    alert(
+        "✅ Application Submitted Successfully!\n\n" +
+        "Application ID: " + applicationId +
+        "\n\nPlease save your Application ID."
+    );
+
+    form.reset();
+});
