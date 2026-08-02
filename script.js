9ca45d844074cd50ebdcf8533350bf06a4126463
HONOURABLEISMAILSABONGARU <banasoyayya01@gmail.com>
2026-08-02 22:55:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -238,3 +238,20 @@ Check Again
 
 `;
 }
+function adminWarning() {
+
+    const proceed = confirm(
+`⚠️ RESTRICTED ACCESS
+
+This area is reserved for the Administrator only.
+
+If you are not the Administrator, click Cancel.
+
+Click OK to continue.`
+    );
+
+    if (proceed) {
+        window.location.href = "admin.html";
+    }
+
+}
